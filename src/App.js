0355bbdbdace2645890dc929ebdd35af96ac66a6
abhaywwe7 <abhaywwe7@gmail.com>
2022-01-30 20:09:26 +0530

    image upload part

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ function App() {
     }
   };
 
+  const uploadImage = (e) => {
+    // console.log(e)
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageURL(url);
+    } else {
+      setImageURL(null);
+    }
+  };
+
   useEffect(() => {
     loadModel();
   }, []);
@@ -25,9 +36,19 @@ function App() {
     return <h2>Model is loading...</h2>;
   }
 
+  console.log(imageURL);
   return (
     <div className="App">
-      <h1>This is an Image Identifier App</h1>
+      <h2 className="header">This is an Image Identifier App</h2>
+      <div className="inputImageHolder">
+        <input
+          type="file"
+          accept="image/"
+          capture="camera"
+          className="uploadInputImage"
+          onChange={uploadImage}
+        />
+      </div>
     </div>
   );
 }
