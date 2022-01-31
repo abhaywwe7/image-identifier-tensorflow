497838b4936c93753da719bc4ff723e643de064b
abhaywwe7 <abhaywwe7@gmail.com>
2022-01-31 14:01:04 +0530

    recent prediction done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ function App() {
   const [isModelLoading, setisModelLoading] = useState(false);
   const [model, setModel] = useState(null);
   const [imageURL, setImageURL] = useState(null);
+  const [results, setResults] = useState([]);
+  const [history, setHistory] = useState([]);
 
   const imageReference = useRef();
-
+  const textInputReference = useRef();
+  const fileInputReference = useRef();
   const loadModel = async () => {
     setisModelLoading(true);
     try {
@@ -31,25 +34,60 @@ function App() {
     }
   };
 
+  const identify = async () => {
+    textInputReference.current.value = "";
+    const results = await model.classify(imageReference.current);
+    // console.log(results);
+    setResults(results);
+  };
+
+  const handleOnChange = (e) => {
+    setImageURL(e.target.value);
+    setResults([]);
+  };
+
+  const triggerUpload = () => {
+    fileInputReference.current.click();
+  };
+
   useEffect(() => {
     loadModel();
   }, []);
+
+  useEffect(() => {
+    if (imageURL) {
+      setHistory([imageURL, ...history]);
+    }
+  }, [imageURL]);
 
   if (isModelLoading) {
     return <h2>Model is loading...</h2>;
   }
 
-  console.log(imageURL);
+  // console.log(imageURL);
+  console.log(results);
+
   return (
     <div className="App">
       <h2 className="header">This is an Image Identifier App</h2>
-      <div className="inputImageHolder">
+      <div className="inputHolder">
         <input
           type="file"
-          accept="image/"
+          accept="image/*"
           capture="camera"
-          className="uploadInputImage"
+          className="uploadInput"
           onChange={uploadImage}
+          ref={fileInputReference}
+        />
+        <button className="uploadImage" onClick={triggerUpload}>
+          Upload Image
+        </button>
+        <span className="or">OR</span>
+        <input
+          type="text"
+          placeholder="Paster image URL"
+          ref={textInputReference}
+          onChange={handleOnChange}
         />
       </div>
       <div className="mainWrapper">
@@ -64,9 +102,48 @@ function App() {
               />
             )}
           </div>
+          {results.length > 0 && (
+            <div className="resultsHolder">
+              {results.map((result, index) => {
+                return (
+                  <div className="result" key={result.className}>
+                    <span className="name">{result.className}</span>
+                    <span className="confidence">
+                      Confidence level: {(result.probability * 100).toFixed(2)}%{" "}
+                      {index === 0 && (
+                        <span className="bestGuess">Best Guess</span>
+                      )}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        {imageURL && <button className="button">Identify</button>}
+        {imageURL && (
+          <button className="button" onClick={identify}>
+            Identify
+          </button>
+        )}
       </div>
+      {history.length > 0 && (
+        <div className="recentPredictions">
+          <h2>Recent Images</h2>
+          <div className="recentImages">
+            {history.map((image, index) => {
+              return (
+                <div className="recentPrediction" key={`${image}${index}`}>
+                  <img
+                    src={image}
+                    alt="Recent Prediction"
+                    onClick={() => setImageURL(image)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
